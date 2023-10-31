6097566f2cf04f3b8a1cabe6f898aec3f76f0b38
wuselbrger <kevinkwu59@gmail.com>
2023-10-31 16:56:28 -0400

    Created and developed card, deck, shuffle, and drawing features

--- Assets/01-Prospector/__Scripts/Card.cs
+++ Assets/01-Prospector/__Scripts/Card.cs
@@ -14,8 +14,44 @@ public class Card : MonoBehaviour {
 	public List<GameObject> pipGOs = new List<GameObject>();
 	
 	public GameObject back;  // back of card;
-	public CardDefinition def;  // from DeckXML.xml		
+	public CardDefinition def;  // from DeckXML.xml	
+	public SpriteRenderer[] spriteRenderers;	
 
+	void Start() {
+		SetSortOrder(0);		
+	}
+	public void PopulateSpriteRenderers(){
+		if(spriteRenderers == null || spriteRenderers.Length == 0){
+			spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+		}
+	} 
+
+	public void SetSortingLayerName(string tSLN){
+		PopulateSpriteRenderers();
+		foreach(SpriteRenderer tSR in spriteRenderers){
+			tSR.sortingLayerName = tSLN;
+		}
+	}
+
+	public void SetSortOrder(int sOrd){
+		PopulateSpriteRenderers(); 
+		
+		foreach(SpriteRenderer tSR in spriteRenderers){
+			if(tSR.gameObject == this.gameObject){
+				tSR.sortingOrder = sOrd;
+				continue;
+			}
+			switch(tSR.gameObject.name){
+				case "back":
+					tSR.sortingOrder = sOrd+2; 
+					break;
+				case "face":
+				default: 
+					tSR.sortingOrder = sOrd+1;
+					break;
+			}
+		}
+	}
 
 	public bool faceUp {
 		get {
@@ -26,18 +62,12 @@ public class Card : MonoBehaviour {
 			back.SetActive(!value);
 		}
 	}
+	 // class Card
 
-
-	// Use this for initialization
-	void Start () {
-	
+	virtual public void OnMouseUpAsButton(){
+		print(name);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-} // class Card
+}
 
 [System.Serializable]
 public class Decorator{
